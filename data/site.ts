7883feeb6afe72f1a4259709f4b29950fe818388
Affax <me@affax.dev>
2020-11-20 22:49:45 +0200

    Added quick & dirty banner

--- data/site.ts
+++ data/site.ts
@@ -2,5 +2,4 @@ export const title = "Cultivator Finance";
 export const description = "Your companion app for Harvest.finance";
 export const url = "cultivator.finance";
 export const logo = "/images/logo/logo.png";
-// TODO: Banner Image
-export const banner = "";
+export const banner = "/images/banner/banner.png";
